c7fa95172d4a2857f54a887a047d67711ea369f4
LLLiuyiC <chen.liu@husky.neu.edu>
2020-02-05 12:01:12 -0800

    week4

---
+++ src/ArticleList/ArticleListItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const ArticleListItem = (props) => {
+  return (
+  <div>
+    <section>
+        <h3>{props.article.title}</h3>
+        <p>{props.article.shortText}</p>
+        <time dateTime={props.article.pubYear}>{props.article.pubDate}</time>
+        <button onClick={() => alert(`${props.article.slug}`)}>
+       show article  slug
+      </button>
+
+    </section>
+
+  </div>
+  );
+}
+
+export default ArticleListItem;
